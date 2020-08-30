1893a75fd6cf96c66bb9e0114b6b26b7ef57f268
A.Kai <alexlain@mail.ru>
2020-08-30 11:46:12 +0300

    вывод списка каталогов и файлов

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/text/number"
 	"io"
+	"io/ioutil"
 	"os"
-	"path/filepath"
 	"strings"
+	//"path/filepath"
+	//"strings"
 )
 
-func dirTree(out io.Reader, path string, printFiles bool) error {
+func dirTree(out io.Writer, path string, printFiles bool) (err error) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
 
+	for _, file := range files {
+		cnt := strings.Count(path, "\\")
+		if file.IsDir() {
+			fmt.Printf("%", file.Name())
+			dirTree(out, path+file.Name()+"\\", printFiles)
+		} else {
+			if printFiles {
+				fmt.Println(file.Name(), printFiles)
+			}
+		}
+	}
+	return
 }
 
 func main() {
@@ -19,6 +38,7 @@ func main() {
 	}
 	path := os.Args[1]
 	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	//fmt.Println(len(os.Args) == 3 && os.Args[2] == "-f")
 	err := dirTree(out, path, printFiles)
 	if err != nil {
 		panic(err.Error())
