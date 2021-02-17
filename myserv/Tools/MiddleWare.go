7bd5a10d3c59c2289b1bd962a04a76ff28710675
Aleksandr-Kai <65232655+Aleksandr-Kai@users.noreply.github.com>
2021-02-17 23:20:42 +0300

    add middleware pkg

---
+++ myserv/Tools/MiddleWare.go
@@ -0,0 +1,46 @@
+package Tools
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"github.com/jackc/pgx"
+)
+
+func MWAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sql.open
+		fmt.Println("adminAuthMiddleware", r.URL.Path)
+		_, err := r.Cookie("session_id")
+		// учебный пример! это не проверка авторизации!
+		if err != nil {
+			fmt.Println("no auth at", r.URL.Path)
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func MWAccessLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("accessLogMiddleware", r.URL.Path)
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		fmt.Printf("[%s] %s, %s %s\n",
+			r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
+	})
+}
+
+func MWPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("panicMiddleware", r.URL.Path)
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println("recovered", err)
+				http.Error(w, "Internal server error", 500)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
